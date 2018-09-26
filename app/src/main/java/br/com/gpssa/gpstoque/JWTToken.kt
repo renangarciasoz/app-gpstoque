43745bfde9d5ca37b8c34810971da6f4c0a3ff71
Renan Garcia <rsgarcia@uolinc.com>
2018-09-26 16:33:01 -0300

    [APP-GPSTOQUE] - Adicionar método de POST para a API encapsulada na Thread

---
+++ app/src/main/java/br/com/gpssa/gpstoque/JWTToken.kt
@@ -0,0 +1,11 @@
+package br.com.gpssa.gpstoque
+
+import android.app.Activity
+import android.os.Bundle
+
+class JWTToken : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
